2f9e4a9c2d0134e6db3910ba461aa08ec2dde6a2
LATBauerdick <github@latbauerdick.de>
2016-08-27 22:59:02 -0500

    restructure

--- lib/vid/vid_store_concrete.ex
+++ lib/vid/vid_store_concrete.ex
@@ -1,4 +1,4 @@
-defmodule VideoStore.Concrete do
+defmodule VidStore.Concrete do
   import StateMachine.Behavior
   def rent(video), do: fire(state_machine, video, :rent)
   def return(video), do: fire(state_machine, video, :return)
@@ -6,15 +6,15 @@ defmodule VideoStore.Concrete do
   def find(video), do: fire(state_machine, video, :find)
   def state_machine do
     [ available: [
-        rent: [ to: :rented, calls: [&VideoStore.renting/1]],
-        rented: [ to: :rented, calls: [&VideoStore.renting/1]]
+        rent: [ to: :rented, calls: [&VidStore.renting/1]],
+        rented: [ to: :rented, calls: [&VidStore.renting/1]]
       ],
       rented: [
-        return: [ to: :available, calls: [&VideoStore.returning/1]],
-        lose: [to: :lost, calls: [&VideoStore.losing/1]]
+        return: [ to: :available, calls: [&VidStore.returning/1]],
+        lose: [to: :lost, calls: [&VidStore.losing/1]]
       ],
       lost: [
-        find: [to: :available, calls: [&VideoStore.finding/1]]
+        find: [to: :available, calls: [&VidStore.finding/1]]
       ]
     ]
   end
--- test/concrete_test.exs
+++ test/concrete_test.exs
@@ -2,16 +2,16 @@ defmodule ConcreteTest do
   use ExUnit.Case
   import Should
   should "should update count" do
-    rented_video = VideoStore.renting(video)
+    rented_video = VidStore.renting(video)
     assert rented_video.times_rented == 1
   end
   should "rent video" do
-    rented_video = VideoStore.Concrete.rent video
+    rented_video = VidStore.Concrete.rent video
     assert :rented == rented_video.state
     assert 1 == Enum.count(rented_video.log)
   end
   should "handle multiple transitions" do
-    import VideoStore.Concrete
+    import VidStore.Concrete
     vid = video |> rent |> return |> rent |> return
                 |> rent |> lose |> find |> rent
     IO.puts " "
